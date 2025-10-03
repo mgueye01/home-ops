98d4d0db061d99c5ccca51b3185f79a764f5d692
Moustapha Gueye <mgueye@mirantis.com>
2025-10-04 01:33:19 +0200

    update config

--- kubernetes/apps/default/slskd/app/resources/beets.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-name=$(echo "$1" | awk -F'"localDirectoryName": "' '{print $2}' | awk -F'",' '{print $1}')
-
-wget -q -O/dev/null \
-     --post-data "name=${name}&path=${name}" \
-     --header="X-API-KEY: ${BETANIN_API_KEY}" \
-     --header="User-Agent: notify-beats.sh" \
-      "http://${BEETS_HOST}/api/torrents"
